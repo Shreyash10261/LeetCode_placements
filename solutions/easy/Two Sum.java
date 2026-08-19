04639b33909ce995d5ea5c57c1cd4937dc2e55ef
Shreyash Shivhare <shivhare.shreyash10@gmail.com>
2026-08-19 16:01:11 +0530

    Add solution for Two Sum

---
+++ solutions/easy/Two Sum.java
@@ -0,0 +1,19 @@
+// Title: Two Sum
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/two-sum/
+
+        {
+            if(map.containsKey(ans))
+            {
+        }
+                return new int[]{map.get(ans), i};
+            }
+    }
+            int ans=target-nums[i];
+            map.put(nums[i], i);
+        return new int[]{};
+        for(int i=0; i<nums.length;i++)
+        HashMap<Integer, Integer> map=new HashMap<>();
+    public int[] twoSum(int[] nums, int target) {
+class Solution {
