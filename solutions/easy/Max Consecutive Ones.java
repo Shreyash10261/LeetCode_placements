a50817d9305fb27533db3e14e959a82064625096
Shreyash Shivhare <shivhare.shreyash10@gmail.com>
2026-08-22 19:14:46 +0530

    Add solution for Max Consecutive Ones

---
+++ solutions/easy/Max Consecutive Ones.java
@@ -0,0 +1,15 @@
+// Title: Max Consecutive Ones
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/max-consecutive-ones/
+
+                count=0;
+            }
+            if(count>max)
+            {
+                max=count;
+            }
+        }
+        return max;
+    }
+}
