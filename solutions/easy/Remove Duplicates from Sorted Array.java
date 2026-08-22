d308de615723a7db700e3549ce45485d2d42549e
Shreyash Shivhare <shivhare.shreyash10@gmail.com>
2026-08-22 17:00:54 +0530

    Add solution for Remove Duplicates from Sorted Array

---
+++ solutions/easy/Remove Duplicates from Sorted Array.java
@@ -0,0 +1,21 @@
+// Title: Remove Duplicates from Sorted Array
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+        {
+        for(int x:set)
+        int i=0;
+        }
+            set.add(nums[i]);
+        {
+        for(int i=0; i<nums.length;i++)
+        TreeSet<Integer> set=new TreeSet<>();
+            nums[i]=x;
+            i++;
+        }
+
+            return set.size();
+        }
+    }
+
