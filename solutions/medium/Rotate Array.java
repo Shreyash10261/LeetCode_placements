e2f943717853419b6a2c46433c4abf5f862d59a6
Shreyash Shivhare <shivhare.shreyash10@gmail.com>
2026-08-22 17:17:45 +0530

    Add solution for Rotate Array

---
+++ solutions/medium/Rotate Array.java
@@ -0,0 +1,18 @@
+// Title: Rotate Array
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/rotate-array/
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int[] r=new int [nums.length];
+        for(int i=0;i<nums.length;i++)
+        {
+            r[(i + k) % nums.length] = nums[i];
+        }
+        for(int i=0; i<nums.length; i++)
+        {
+            nums[i]=r[i];
+        }
+    }
+}
