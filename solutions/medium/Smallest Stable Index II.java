9981e038bb392809fe7845e7fbc0dd56eee398d7
Shreyash Shivhare <shivhare.shreyash10@gmail.com>
2026-09-05 22:50:39 +0530

    Add solution for Smallest Stable Index II

---
+++ solutions/medium/Smallest Stable Index II.java
@@ -0,0 +1,20 @@
+// Title: Smallest Stable Index II
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/smallest-stable-index-ii/
+
+            max = Math.max(max, nums[i]);
+        {
+        int max = nums[0];
+        for (int i = 0; i < n; i++) 
+
+            minArray[i] = Math.min(nums[i], minArray[i + 1]);
+        }
+        {
+        for (int i = n - 2; i >= 0; i--) 
+
+        minArray[n - 1] = nums[n - 1];
+        int[] minArray = new int[n];
+        int n = nums.length;
+    public int firstStableIndex(int[] nums, int k) {
+class Solution {
