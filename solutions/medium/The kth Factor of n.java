4de5c980f532d05abae76d3d6c9d36fc7d91067c
Shreyash Shivhare <shivhare.shreyash10@gmail.com>
2026-09-12 17:31:57 +0530

    Add solution for The kth Factor of n

---
+++ solutions/medium/The kth Factor of n.java
@@ -0,0 +1,20 @@
+// Title: The kth Factor of n
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/the-kth-factor-of-n/
+
+        if(n%i==0)
+        {
+        }
+       }
+            k--;
+            if(k==0)
+            {
+                return i;
+            }
+    }
+       return -1;
+}
+       {
+       for(int i=1;i<=n;i++)
+    public int kthFactor(int n, int k) {
