a3f435ba660c9c19502f2f505289a24073d0ea85
Shreyash Shivhare <shivhare.shreyash10@gmail.com>
2026-09-12 19:36:44 +0530

    Add solution for Sum of Square Numbers

---
+++ solutions/medium/Sum of Square Numbers.java
@@ -0,0 +1,19 @@
+// Title: Sum of Square Numbers
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/sum-of-square-numbers/
+
+
+        for(long a = 0; a * a <= c; a++)
+        {
+            long b = (long)Math.sqrt(c - a * a);
+
+            if(a * a + b * b == c)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
