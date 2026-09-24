62da4c2f1fb7ffcdad93d512734aa939f76dcd4c
Shreyash Shivhare <shivhare.shreyash10@gmail.com>
2026-09-24 16:46:09 +0530

    Add solution for Smallest Index With Digit Sum Equal to Index

---
+++ solutions/easy/Smallest Index With Digit Sum Equal to Index.java
@@ -0,0 +1,20 @@
+// Title: Smallest Index With Digit Sum Equal to Index
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/smallest-index-with-digit-sum-equal-to-index/
+
+                num /= 10;
+            }
+
+            if (sum == i) 
+                sum += num % 10;
+            {
+            while (num > 0) 
+
+        for (int i = 0; i < nums.length; i++) 
+        {
+            int num = nums[i];
+            int sum = 0;
+    public int smallestIndex(int[] nums) {
+
+class Solution {
