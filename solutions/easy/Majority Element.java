631431efbb18ec1543843c4f149361a70ddcef01
Shreyash Shivhare <shivhare.shreyash10@gmail.com>
2026-09-24 16:53:28 +0530

    Add solution for Majority Element

--- solutions/easy/Majority Element.java
+++ solutions/easy/Majority Element.java
@@ -3,10 +3,24 @@
             // Language: Java
             // Link: https://leetcode.com/problems/majority-element/
 
-class Solution {
+        int n=nums.length;
+        HashMap<Integer, Integer> map=new HashMap<>();
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-    }
-        int n=nums.length/2;
-        return nums[n];
-}
+class Solution {
+        for(int i=0;i<n;i++)
+        {
+            if(map.containsKey(nums[i]))
+            {
+                map.put(nums[i], map.get(nums[i])+1);
+            }
+            else
+            {
+                map.put(nums[i], 1);
+            }
+        }
+        for(int key:map.keySet())
+        {
+            if(map.get(key)>n/2)
+            {
+                return key;
+            }
